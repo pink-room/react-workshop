8c031bcfa28598404133a33bfedc04824e0ad365
FranciscoJRSantos <franciscojrsantos1997@gmail.com>
2018-10-24 16:46:12 +0100

    B1: Change API method.

--- src/core/api.js
+++ src/core/api.js
@@ -1,15 +1,14 @@
 const API_URL = 'https://api.imgflip.com/';
 
-// get data from http://version1.api.memegenerator.net/Generators_Select_ByPopular
+// get memes from https://api.imgflip.com/get_memes
 export function getPopularImages() {
   return request('get_memes');
 }
 
-// request to create a meme at http://version1.api.memegenerator.net/Instance_Create?generatorID=74&languageCode=en&text0=I%20don%27t%20always%20make%20memes&text1=but%20when%20I%20do%20it%27s%20at%20a%20workshop
-export function createMeme(generatorID, text0, text1) {
-  return request('Instance_Create', {
-    languageCode: 'en',
-    generatorID,
+// request to create a meme at https://api.imgflip.com/caption_image
+export function createMeme(template_id, text0, text1) {
+  return request('caption_image', {
+    template_id,
     text0,
     text1
   });

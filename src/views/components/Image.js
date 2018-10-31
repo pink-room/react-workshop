9fec9abbdcd2836f05be7114df5163ce2434ca25
FranciscoJRSantos <franciscojrsantos1997@gmail.com>
2018-10-31 15:44:46 +0000

    B2: Create Image Component and add styling to it.

--- src/views/components/Image.js
+++ src/views/components/Image.js
@@ -1,3 +1,12 @@
 import React from 'react';
+import 'views/assets/stylesheets/Image.css';
 
-export const Image = (props) => { }
+export const Image = ({ name, url, selected }) => {
+  const selectedClass = selected ? 'selected' : '';
+  return (
+    <div className={`Image ${selectedClass}`}>
+      <span>{name}</span>
+      <img src={url} alt='nice meme' />
+    </div>
+  );
+};

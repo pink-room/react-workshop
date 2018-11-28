7182117c0cc6c92ef30668904053e789b9a949c8
FranciscoJRSantos <franciscojrsantos1997@gmail.com>
2018-11-28 11:04:03 +0000

    B3: Change ternary operator results for readability.

--- src/views/components/Image.js
+++ src/views/components/Image.js
@@ -3,10 +3,10 @@ import 'views/assets/stylesheets/Image.css';
 
 export const Image = ({image, selectedImageUrl, handleClick}) => {
   const selectedClass =
-    selectedImageUrl && selectedImageUrl === image.url ? 'selected' : '';
+    selectedImageUrl && selectedImageUrl === image.url ? 'Image selected' : 'Image';
   return (
     <div
-      className={`Image ${selectedClass}`}
+      className={`${selectedClass}`}
       onClick={e => handleClick(image, e)}>
       <span>{image.name}</span>
       <img src={image.url} alt="nice meme" />
